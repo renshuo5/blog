cf174cb50e3d9ed58449937acf676c6571c04a30
任硕 <shuo.ren@paiyue.com>
2016-08-18 18:47:52 +0800

    增加model模型

--- blog-manage-war/src/main/java/com/blog/manage/controller/CommonController.java
+++ blog-manage-war/src/main/java/com/blog/manage/controller/CommonController.java
@@ -10,33 +10,31 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.rs.user.entity.User;
+import com.rs.user.model.LoginInfo;
+import com.rs.user.service.UserService;
 
 @ControllerAdvice("com.blog.manage")
 public class CommonController {
 	
 	protected transient final Logger logger = LoggerFactory.getLogger(this
 			.getClass());
+	
 	@Autowired
-//	private UserService userService;
+	private UserService userService;
 	
 	public static final String ATTR_LOGIN_USER = "loginUser";
 
-//	@ModelAttribute(ATTR_LOGIN_USER)
-//	public User loginUser(HttpServletRequest req) {
-//		
-//		User user = (User) SecurityUtils.getSubject().getPrincipal();
+	@ModelAttribute(ATTR_LOGIN_USER)
+	public User loginUser(HttpServletRequest req) {
 		
-//		LoginInfo login = null;
-//		if(!ClientInfoHolder.isHealthCheck(req)){
-//			login = (LoginInfo) SecurityUtils.getSubject().getPrincipal();
-//		}
-//		if (login != null) {
-//			return userService.findOne(login.getUserId());
-//		} else {
-//			return null;
-//		}
+		LoginInfo login = (LoginInfo) SecurityUtils.getSubject().getPrincipal();
+		if (login != null) {
+			return userService.findOne(login.getUserId());
+		} else {
+			return null;
+		}
 		
-//		return user;
-//	}
+		
+	}
 
 }
