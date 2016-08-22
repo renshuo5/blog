5c71a4d39d613f81dff1ae5cd26aa82644ff517a
renshuo <825558984@qq.com>
2016-08-22 23:58:11 +0800

    修改新闻实体类

--- blog-model/src/main/java/com/blog/news/entity/News.java
+++ blog-model/src/main/java/com/blog/news/entity/News.java
@@ -2,12 +2,20 @@ package com.blog.news.entity;
 
 import java.sql.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import com.blog.member.entity.Member;
 import com.rensframework.core.entity.Auditable;
 import com.rensframework.core.entity.TimeEntity;
 
@@ -22,8 +30,11 @@ public class News extends TimeEntity implements Auditable<News> {
 	 */
 	private static final long serialVersionUID = -5599236497070947983L;
 
+	@NotNull
 	private String title;
 	
+	@Column(insertable=false,updatable=false,nullable=false ,length=6000)
+	@NotNull
 	private String content;
 	/**
 	 * 发布日期
@@ -32,6 +43,13 @@ public class News extends TimeEntity implements Auditable<News> {
 	private Date pubDate;
 	
 	
+	@Column(insertable=false,updatable=false,nullable=true)
+	private Long memberId;
+	
+	@ManyToOne(cascade=CascadeType.REFRESH,fetch=FetchType.LAZY)
+	@JoinColumn(name="memberId")
+	@NotNull
+	private Member member;
 	
 	@Override
 	public String getEntityName() {
